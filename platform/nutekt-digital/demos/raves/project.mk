b3670f3b22d68b4d87db3e6082fc9346e68bbf5d
Aaron Tomb <atomb@galois.com>
2020-10-26 12:38:07 -0700

    First version of "raves" oscillator
    This includes infrastructure for including Rust code, but does not yet
    call any Rust.

---
+++ platform/nutekt-digital/demos/raves/project.mk
@@ -0,0 +1,18 @@
+# #############################################################################
+# Project Customization
+# #############################################################################
+
+PROJECT = waves
+
+UCSRC = 
+
+UCXXSRC = waves.cpp
+
+UINCDIR =
+
+UDEFS = -fno-threadsafe-statics
+
+#ULIBS = -lfoo
+ULIBS =
+
+ULIBDIR = rust
